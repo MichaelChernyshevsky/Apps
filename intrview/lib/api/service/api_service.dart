e42728592bae285eb143793141c9547e2782eef5
Mischa <102793012+MichaelChernyshevsky@users.noreply.github.com>
2023-10-26 02:09:43 +0300

    delete interview

--- intrview/lib/api/service/api_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:app_with_apps/api/connections/whether_api.dart';
-
-class ServiceApiNotes {
-  final WhetherApi _whetherApi = WhetherApi();
-
-  Future<String> getWhether() async => await _whetherApi.getWhether();
-}
